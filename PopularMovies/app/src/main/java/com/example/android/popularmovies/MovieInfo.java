0f858af27c849f44c30f3a164b1c3d6422c209e1
Travis Carr <Travis Carr>
2017-05-13 15:12:40 -0500

    Add MovieInfo class, updated main activity layout to be recyclerView, and added movie_list_item.xml layout

---
+++ PopularMovies/app/src/main/java/com/example/android/popularmovies/MovieInfo.java
@@ -0,0 +1,13 @@
+package com.example.android.popularmovies;
+
+/**
+ * Created by Travis on 5/13/2017.
+ */
+
+public class MovieInfo {
+    String movieTitle;
+
+    public MovieInfo (String movieTitle) {
+        this.movieTitle = movieTitle;
+    }
+}
