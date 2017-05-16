b6a07c7816d39b1656b1583c5e945c6ab6b3d110
Travis Carr <Travis Carr>
2017-05-16 06:19:39 -0500

    Added all movieInfo to the detail activity.  Updated layout of detail activity.

--- PopularMovies/app/src/main/java/com/example/android/popularmovies/DetailActivity.java
+++ PopularMovies/app/src/main/java/com/example/android/popularmovies/DetailActivity.java
@@ -3,21 +3,36 @@ package com.example.android.popularmovies;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import org.w3c.dom.Text;
 
 public class DetailActivity extends AppCompatActivity {
     private static final String TAG = DetailActivity.class.getSimpleName();
 
-    private TextView mTestTextView;
+    private TextView mTitleTextView;
+    private ImageView mImagePosterImageView;
+    private TextView mPlotTextView;
+    private TextView mReleaseDateTextView;
+    private TextView mUserRatingTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        mTestTextView = (TextView) findViewById(R.id.tv_detail_test);
+        mTitleTextView = (TextView) findViewById(R.id.tv_detail_title);
+
+        mImagePosterImageView = (ImageView) findViewById(R.id.iv_detail_poster);
+
+        mPlotTextView = (TextView) findViewById(R.id.tv_detail_plot);
+
+        mReleaseDateTextView = (TextView) findViewById(R.id.tv_detail_date);
+
+        mUserRatingTextView = (TextView) findViewById(R.id.tv_detail_rating);
 
         Intent intentThatStartedThisActivity = getIntent();
 
@@ -26,7 +41,15 @@ public class DetailActivity extends AppCompatActivity {
             MovieInfo movieInfo = bundle.getParcelable("parseMovieInfo");
 
             if (movieInfo != null) {
-                mTestTextView.setText(movieInfo.moviePosterImage);
+                mTitleTextView.setText(movieInfo.originalTitle);
+                mPlotTextView.setText(movieInfo.plotSynopsis);
+                //TODO need to change to show the year
+                mReleaseDateTextView.setText(movieInfo.releaseDate);
+                mUserRatingTextView.setText(movieInfo.userRating + "/10");
+
+               Picasso.with(this)
+                        .load(movieInfo.moviePosterImage)
+                        .into(mImagePosterImageView);
             }
         }
 
